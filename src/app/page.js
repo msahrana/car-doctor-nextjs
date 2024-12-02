8be697b6ca79c89fd27bb6b2a1fe39c71872238a
msahrana <msahrana@gmail.com>
2024-12-03 01:52:56 +0600

    about page is create successfully

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import About from "@/components/HomePage/About";
 import Banner from "@/components/HomePage/Banner";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <About />
     </div>
   );
 }
--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -1,19 +1,90 @@
 import React from "react";
 
 const Banner = () => {
+  const bannars = [
+    {
+      title: "Affordable Price For Car Servicing",
+      description:
+        "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+      next: "#slide2",
+      prev: "#slide6",
+    },
+    {
+      title: "Affordable Price For Car Servicing",
+      description:
+        "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+      next: "#slide3",
+      prev: "#slide1",
+    },
+    {
+      title: "Affordable Price For Car Servicing",
+      description:
+        "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+      next: "#slide4",
+      prev: "#slide2",
+    },
+    {
+      title: "Affordable Price For Car Servicing",
+      description:
+        "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+      next: "#slide5",
+      prev: "#slide3",
+    },
+    {
+      title: "Affordable Price For Car Servicing",
+      description:
+        "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+      next: "#slide6",
+      prev: "#slide4",
+    },
+    {
+      title: "Affordable Price For Car Servicing",
+      description:
+        "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+      next: "#slide1",
+      prev: "#slide5",
+    },
+  ];
+
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <div>Lorem ipsum dolor sit amet.</div>
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
+    <div className="carousel w-full my-5 text-white">
+      {bannars.map((bannar, index) => (
+        <div
+          key={index}
+          style={{
+            backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7), rgba(0,0,0,0.3)), url(/assets/images/banner/${
+              index + 1
+            }.jpg)`,
+          }}
+          id={`slide${index + 1}`}
+          className="carousel-item relative w-full bg-top bg-no-repeat h-[70vh] rounded-xl"
+        >
+          <div className="w-full h-full flex items-center pl-36">
+            <div className="space-y-5">
+              <h1 className="text-6xl font-bold w-1/2">{bannar.title}</h1>
+              <p className="w-1/2">{bannar.description}</p>
+              <button className="btn btn-primary mr-4">Discover More</button>
+              <button className="btn btn-outline text-white">
+                Latest Project
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform bottom-12 right-12">
+            <a
+              href={bannar.prev}
+              className="btn btn-circle mr-6 hover:bg-primary border-none"
+            >
+              ❮
+            </a>
+            <a
+              href={bannar.next}
+              className="btn btn-circle hover:bg-primary border-none"
+            >
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
